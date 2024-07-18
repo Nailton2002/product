988e6724d61ea1322eef0b128960546cd395751c
Nailton2002 <jnmp25jnmp@gmail.com>
2024-07-18 17:22:52 -0300

    Camada de serviços - metodos findAll e findById

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from '../model/produto';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Produto[]>{
+    return this.http.get<Produto[]>(`${API_CONFIG.baseUrl}/products`)
+  }
+
+  findById(produto: Produto): Observable<Produto>{
+    return this.http.get<Produto>(`${API_CONFIG.baseUrl}/products/${produto.id}`);
+  }
+  
 }
